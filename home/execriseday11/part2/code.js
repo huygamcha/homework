ce53c9089ae0d37dc31cc9c576d55ffb31999eb8
huygamcha <lehuynhhuy2002@gmail.com>
2023-06-17 17:56:49 +0700

    up

--- home/execriseday11/part2/code.js
+++ home/execriseday11/part2/code.js
@@ -2,28 +2,27 @@ const modalBtn = document.querySelector('.modalhand');
 const barBtn = document.querySelector('.content-select')
 const closeBtn = document.querySelector('.modal-header-close')
 const contentBtn = document.querySelector('.modal-content')
-const downBtns = document.querySelectorAll('.modal-item-down')
-function opened(){
+const downBtn = document.querySelector('.modal-item-down')
+function opened() {
     modalBtn.classList.add('open');
-  
+
 }
-function closed(){
+function closed() {
     modalBtn.classList.remove('open');
-  
+    document.querySelector('.modal-item-body').classList.remove("open");
+
 }
 barBtn.addEventListener('click', opened);
 closeBtn.addEventListener('click', closed);
 modalBtn.addEventListener('click', closed);
-contentBtn.addEventListener('click', function(e) {
+contentBtn.addEventListener('click', function (e) {
     e.stopPropagation();
 })
 // sản phẩm down trong phần nav thu gọn
-for (downBtn in downBtns){
-    
-}
-downBtn.addEventListener('click',   function() {
-     
+
+downBtn.addEventListener('click', function () {
+
     document.querySelector('.modal-item-body').classList.toggle("open");
-      
-}  )
+
+})
 
